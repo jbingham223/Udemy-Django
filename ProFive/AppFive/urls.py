780ab08aa60eb675533fea72f3cd98f813fdcffb
Jason Bingham <jasonbingham@Jasons-MacBook-Air.local>
2022-01-20 20:52:30 -0300

    first Udemy project

---
+++ ProFive/AppFive/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from AppFive import views
+
+app_name = 'AppFive'
+
+urlpatterns = [
+    path('user_login/',views.user_login,name='user_login'),
+    path('register/',views.register,name='register'),
+]
