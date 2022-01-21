b1a3cd1cca1d4809583ae58cfde284d1501ee3e4
jbingham223 <30755601+jbingham223@users.noreply.github.com>
2022-01-21 15:07:57 -0300

    Update settings.py
    Added pythonanywhere to ALLOWED_HOSTS

--- ProFive/ProFive/settings.py
+++ ProFive/ProFive/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-m0js562pd3+h2%4tw+2_8@fyuhel$q2z%@50p0k*oo#68tuuh-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jbingham223.pythonanywhere.com',]
 
 
 # Application definition
